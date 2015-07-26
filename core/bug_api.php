0928d0aeb684b7bc2e0be2afc75ff6cca1f210c5
root <root@trmsrv.dsk.loc>
2015-07-27 02:09:26 +0300

    make mantis replace anonymous reporter

--- core/bug_api.php
+++ core/bug_api.php
@@ -518,7 +518,11 @@ class BugData {
 		} else {
 			$t_status = $this->status;
 		}
-
+	
+		$t_replace_reporter_id = $this->reporter_id;
+		if( $t_replace_reporter_id == user_get_id_by_name( config_get( 'anonymous_account' ) ) ) {
+			$t_replace_reporter_id = user_get_id_by_name( config_get( 'anonymous_account_replacer' ) );
+		}
 		# Insert the rest of the data
 		db_param_push();
 		$t_query = 'INSERT INTO {bug}
@@ -538,7 +542,8 @@ class BugData {
 					      ' . db_param() . ',' . db_param() . ',' . db_param() . ',' . db_param() . ',
 					      ' . db_param() . ',' . db_param() . ',' . db_param() . ',' . db_param() . ',
 					      ' . db_param() . ',' . db_param() . ',' . db_param() . ',' . db_param() . ')';
-		db_query( $t_query, array( $this->project_id, $this->reporter_id, $this->handler_id, $this->duplicate_id, $this->priority, $this->severity, $this->reproducibility, $t_status, $this->resolution, $this->projection, $this->category_id, $this->date_submitted, $this->last_updated, $this->eta, $t_text_id, $this->os, $this->os_build, $this->platform, $this->version, $this->build, $this->profile_id, $this->summary, $this->view_state, $this->sponsorship_total, $this->sticky, $this->fixed_in_version, $this->target_version, $this->due_date ) );
+
+		db_query( $t_query, array( $this->project_id, $t_replace_reporter_id, $this->handler_id, $this->duplicate_id, $this->priority, $this->severity, $this->reproducibility, $t_status, $this->resolution, $this->projection, $this->category_id, $this->date_submitted, $this->last_updated, $this->eta, $t_text_id, $this->os, $this->os_build, $this->platform, $this->version, $this->build, $this->profile_id, $this->summary, $this->view_state, $this->sponsorship_total, $this->sticky, $this->fixed_in_version, $this->target_version, $this->due_date ) );
 
 		$this->id = db_insert_id( db_get_table( 'bug' ) );
 
