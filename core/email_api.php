87894338f45f191b000f9936865c740fa27b2be3
root <badfiles@mail.ru>
2016-06-20 15:18:11 +0300

    hide history from e-mails

--- core/email_api.php
+++ core/email_api.php
@@ -1599,7 +1599,7 @@ function email_format_bug_message( array $p_visible_bug_data ) {
 
 	$t_message .= $t_email_separator1 . " \n";
 	$t_message .= $p_visible_bug_data['freetext']  . "\n";
-
+/**
 	# format history
 	if( array_key_exists( 'history', $p_visible_bug_data ) ) {
 		$t_message .= lang_get( 'bug_history' ) . " \n";
@@ -1614,7 +1614,7 @@ function email_format_bug_message( array $p_visible_bug_data ) {
 		}
 		$t_message .= $t_email_separator1 . " \n\n";
 	}
-
+*/
 	return $t_message;
 }
 
