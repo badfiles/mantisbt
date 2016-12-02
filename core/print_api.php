3b6389ae4539d8eb3d013f44abffbde1221a69db
root <badfiles@mail.ru>
2016-12-02 18:20:06 +0300

    replace deprecated call

--- core/print_api.php
+++ core/print_api.php
@@ -1964,13 +1964,13 @@ function print_bug_attachment_header( array $p_attachment, $p_security_token ) {
 
 	if( access_compare_level( current_user_get_access_level( ), config_get( 'send_attachments_threshold' ) ) ) {
 		echo lang_get( 'word_separator' ) . '&#160;&#160;';
-		print_button( 'bug_file_toggle.php?action=s&file_id=' . $p_attachment['id'] . form_security_param( 'bug_file_to_send_toggle' ),
+		print_link_button( 'bug_file_toggle.php?action=s&file_id=' . $p_attachment['id'] . form_security_param( 'bug_file_to_send_toggle' ),
 			 lang_get( $p_attachment['to_send'] ? 'rem_send_link' : 'set_send_link' ), 'btn-xs' );
 	}
 
 	if( access_compare_level( current_user_get_access_level( ), config_get( 'handle_protected_attachments_threshold' ) ) ) {
 		echo lang_get( 'word_separator' ) . '&#160;&#160;';
-		print_button( 'bug_file_toggle.php?action=l&file_id=' . $p_attachment['id'] . form_security_param( 'bug_file_protected_toggle' ),
+		print_link_button( 'bug_file_toggle.php?action=l&file_id=' . $p_attachment['id'] . form_security_param( 'bug_file_protected_toggle' ),
 			 lang_get( $p_attachment['protected'] ? 'rem_lock_link' : 'set_lock_link' ), 'btn-xs' );
 	}
 }
