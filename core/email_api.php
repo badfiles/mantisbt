1405b198f79a7da484e10b059e7665b65afebc1e
Vladimir Shulev <badfiles@mail.ru>
2019-04-13 22:32:44 +0300

    fix merge

--- core/email_api.php
+++ core/email_api.php
@@ -1340,13 +1340,8 @@ function email_send( EmailData $p_email_data ) {
 		$t_mail->DKIM_identity = config_get( 'email_dkim_identity' );
 	}
 
-<<<<<<< HEAD
-	$t_mail->IsHTML( false );             # set email format to plain text
+	$t_mail->isHTML( false );             # set email format to plain text
 	$t_mail->WordWrap = 300;              # set word wrap to 300 characters
-=======
-	$t_mail->isHTML( false );              # set email format to plain text
-	$t_mail->WordWrap = 80;              # set word wrap to 80 characters
->>>>>>> master
 	$t_mail->CharSet = $t_email_data->metadata['charset'];
 	$t_mail->Host = config_get( 'smtp_host' );
 	$t_mail->From = config_get( 'from_email' );
